36d88845fadfdffcf81002bdc653b77163b45b33
qujinwei <qujinwei@tal.com>
2021-01-13 18:51:50 +0800

    构造函数的继承方式

--- test.js
+++ test.js
@@ -1,14 +1,28 @@
-function Super (){
-    this.colors = ['red','blue','green']
+function Super (name){
+    this.name = name
+    this.colors =  ["red", "blue", "green"]
+}
+Super.prototype.sayName = function(){
+    console.log(this.name);
 }
 
-function Sub (){}
-// 让Sub继承Super
+function Sub (name, age){
+    // 继承Super
+    Super.call(this, name)
+    this.age = age
+}
 Sub.prototype = new Super()
+Sub.prototype.sayAge = function(){
+    console.log(this.age);
+}
+Sub.prototype.constructor = Sub
 
-let instance1 = new Sub()
+let instance1 = new Sub('小明', 15)
 instance1.colors.push('black')
-console.log(instance1.colors);
+console.log(instance1.colors);   //[ 'red', 'blue', 'green', 'black' ]
+instance1.sayName() //小明
+instance1.sayAge()  //15
 
-let instance2 = new Sub()
-console.log(instance2.colors);
+let instance2 = new Sub('小王', 18)
+instance2.sayName() //小王
+instance2.sayAge()  //18
